0ffead2dd9c877436ac12ef85e8f8dac41aa7c37
GuiGoVaz <guilhermegomesvaz@gmail.com>
2026-06-03 13:44:29 -0300

    identity service finished

--- identity-service/src/controllers/identity-controller.js
+++ identity-service/src/controllers/identity-controller.js
@@ -1,7 +1,8 @@
 const logger = require("../utils/logger");
 const User = require("../models/User");
-const validateRegistration = require("../utils/validation");
+const { validateRegistration, validateLogin } = require("../utils/validation");
 const generateTokens = require("../utils/generateToken");
+const RefreshToken = require("../models/RefreshToken");
 //user registration
 const registerUser = async (req, res) => {
   logger.info("Registration endpoint hit");
@@ -45,9 +46,130 @@ const registerUser = async (req, res) => {
 };
 
 //user login
+const loginUser = async (req, res) => {
+  logger.info("Login endpoint hit...");
+  try {
+    const { error } = validateLogin(req.body);
+    if (error) {
+      logger.warn("Validation error", error.details[0].message);
+      return res.status(400).json({
+        success: false,
+        message: error.details[0].message,
+      });
+    }
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      logger.warn("Invalid user");
+      return res.status.json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+    //check if password is valid
+    const isValidPassword = await user.comparePassword(password);
+    if (!isValidPassword) {
+      logger.warn("Invalid password");
+      return res.status.json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const { accessToken, refreshToken } = await generateTokens(user);
+    res.json({ accessToken, refreshToken, userId: user._id });
+  } catch (err) {
+    logger.error("Login error occured", err);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error ",
+    });
+  }
+};
 
 //refresh token
+const refreshTokenController = async (req, res) => {
+  logger.info("Refresh token endpoint hit...");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      logger.warn("Refresh token missing");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token missing",
+      });
+    }
+
+    const storedToken = await RefreshToken.findOne({ token: refreshToken });
+
+    if (!storedToken || storedToken.expiresAt < new Date()) {
+      logger.warn("Invalid or expired refresh token");
+      return res.status(401).json({
+        success: false,
+        message: "Invalid or expired refresh token",
+      });
+    }
+
+    const user = await User.findById(storedToken.user);
+
+    if (!user) {
+      logger.warn("User not found");
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+      await generateTokens(user);
+
+    //delete the old refresh token
+    await RefreshToken.deleteOne({ _id: storedToken._id });
+
+    res.json({
+      accessToken: newAccessToken,
+      refreshToken: newRefreshToken,
+    });
+  } catch (err) {
+    logger.error("Refresh token error occured", err);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error ",
+    });
+  }
+};
 
 //logout
+const logoutUser = async (req, res) => {
+  logger.info("Logout endpoint hit...");
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) {
+      logger.warn("Refresh token missing");
+      return res.status(400).json({
+        success: false,
+        message: "Refresh token missing",
+      });
+    }
+    await RefreshToken.deleteOne({ token: refreshToken });
+    logger.info("Refresh token deleted for logout");
+    res.json({
+      success: true,
+      message: "Logout successfully",
+    });
+  } catch (err) {
+    logger.error("Logout error occured", err);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error ",
+    });
+  }
+};
 
-module.exports = { registerUser };
+module.exports = {
+  registerUser,
+  loginUser,
+  refreshTokenController,
+  logoutUser,
+};
